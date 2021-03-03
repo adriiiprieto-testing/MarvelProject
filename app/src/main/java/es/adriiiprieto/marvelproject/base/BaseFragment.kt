edaa6b5803e9d4724c38e7d0e195e8bf3d3fdd3b
adriiiprieto <adriiiprieto@gmail.com>
2021-03-03 09:54:00 +0100

    R001 Release version v1.0.0

--- app/src/main/java/es/adriiiprieto/marvelproject/base/BaseFragment.kt
+++ app/src/main/java/es/adriiiprieto/marvelproject/base/BaseFragment.kt
@@ -45,6 +45,9 @@ abstract class BaseFragment<VS : BaseViewState, VM : BaseViewModel<VS>, B : View
         // Get or create the viewModel
         viewModel = ViewModelProvider(this).get(viewModelClass)
 
+        // Setup view sending the View Model
+        setupView(viewModel)
+
         // Setup observers
         viewModel.getObservableState().observe(viewLifecycleOwner, { state ->
             onNormal(state.data)
@@ -55,9 +58,6 @@ abstract class BaseFragment<VS : BaseViewState, VM : BaseViewModel<VS>, B : View
                 }
             }
         })
-
-        // Setup view sending the View Model
-        setupView(viewModel)
 
         // Fragment start
         viewModel.onStart()
